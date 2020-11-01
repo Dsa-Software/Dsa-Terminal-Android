2d8202132145f6ba3adbf3ec4e236e671c2c5c7a
Felipe Souza Pereira Lima <eersc12@gmail.com>
2020-11-01 10:35:17 -0300

    Tue SYstem

---
+++ src/Terminal.java
@@ -0,0 +1,8 @@
+public class Terminal {
+    public static void main(String[] args) {
+        System.out.println("Iniciando Dsa Terminal. . .");
+        while (true){
+            System.out.println("I love");
+        }
+    }
+}
