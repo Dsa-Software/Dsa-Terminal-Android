bc654f2ce5a73df26d971b306222e7cbf105526f
Felipe Souza Pereira Lima <eersc12@gmail.com>
2020-11-03 20:16:22 -0300

    Update main.java

--- scr/main.java
+++ scr/main.java
@@ -1,33 +1,20 @@
-/*
-    Dsa Terminal Android v0.0.01
-    (C) 2020 Dsa Software Fundation, all directs reserved | Mit License
-*/
-public class main{
-    // terminal Code
-    public static void main(String[], args){
-        // Version of Dsa Terminal
+public class Main{
+    public static void main(String[] args){
         var __version__ = "0.0.01";
-        // Start of Dsa Terminal
         System.out.println("Starting Dsa Terminal. . .");
         System.out.println("(C) 2020 Dsa Terminal v" + __version__);
-        // System of Terminal
-        while (true)
-        {
-            // Command Prompt
+        while (true){
             var cmd = "";
-            // Exit of Dsa Terminal
-            else if (cmd == "exit"){
+            if (cmd == "exit"){
                 System.out.println("Exiting Dsa Terminal. . . .");
                 break;
             }
-            // Help
-            if (cmd == "help"){
+            else if (cmd == "help"){
                 System.out.println("Dsa Terminal Help:");
                 System.out.println("Command:           Description:");
                 System.out.println("exit               Exit of Dsa Terminal");
                 System.out.println("help               Help");
             }
-            // Command not found
             else{
                 System.out.println(cmd + ": Command not found!");
             }
